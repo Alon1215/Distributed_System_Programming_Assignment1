8b52b5d80322bab2aff18625b7d3d484e6650217
HelloWorld <michalon@post.bgu.ac.il>
2020-11-23 19:28:14 +0200

    created html handler to parse result

--- src/main/java/Manager/HTMLHandler.java
+++ src/main/java/Manager/HTMLHandler.java
@@ -7,48 +7,32 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Vector;
 
 public class HTMLHandler {
 
-    public static final String HTML_HEADER= "<html>\n\t<head>\n\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\"><title>OCR</title>\n\t</head>\n\t<body>\n";
-    public static final String HTML_FOOTER = "\t</body>\n</html>";
-    public static final String HTML_START_PARAGRAPH_AND_IMAGE = "\t\t<p>\n" +"\t\t<img src=";
-    public static final String HTML_AFTER_IMAGE_BEFORE_TEXT = "><br>\n\t\t";
-    public static final String HTML_AFTER_TEXT = "\n\t\t</p>\n";
+    public static void parseListOfUrlAndTextToHTML(Vector<Pair<String,String>> urlsToText, String replyUrl){
 
-
-    public HTMLHandler() {
-    }
-
-    public void parseListOfUrlAndTextToHTML(Vector<Pair<String,String>> urlsToText, String fileName){
-        File f = new File(fileName+".html");
+        File f = new File(replyUrl+".html");
         try{
             BufferedWriter bw = new BufferedWriter(new FileWriter(f));
-            bw.write(HTML_HEADER);
+            bw.write("<html>\n\t<head>\n\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\"><title>OCR</title>\n\t</head>\n\t<body>\n");
             for(Pair<String, String> p : urlsToText){
 
-                String paragraph = HTML_START_PARAGRAPH_AND_IMAGE + p.getKey() +
-                        HTML_AFTER_IMAGE_BEFORE_TEXT +
+                String paragraph = "\t\t<p>\n" +"\t\t<img src=" + p.getKey() +
+                        "><br>\n\t\t" +
                         p.getValue() +
-                        HTML_AFTER_TEXT;
+                        "\n\t\t</p>\n";
                 bw.write(paragraph);
             }
-            bw.write(HTML_FOOTER);
+            bw.write("\t</body>\n</html>");
             bw.close();
         }catch (IOException e){
             System.out.println("problem with io");
             e.printStackTrace();
         }
-    }
-
-
-    public static void main(String[] args) {
-        HTMLHandler parser = new HTMLHandler();
-        Map<String,String> input = new HashMap<>();
-        input.put("\"http://ct.mob0.com/Fonts/CharacterMap/ocraextended.png\"","...TEXT THAT WAS PARSED...");
-        parser.parseListOfUrlAndTextToHTML(input,"output");
     }
 
 
